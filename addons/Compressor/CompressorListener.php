8aedd4537e05282794d55ced807fb7358cdabaf4
Than Tibbetts <than@dirigible.us>
2016-01-18 16:26:00 -0600

    documenting code

--- addons/Compressor/CompressorListener.php
+++ addons/Compressor/CompressorListener.php
@@ -14,6 +14,8 @@ class CompressorListener extends Listener
     {
         // $path = /path/to/local/cache/glide/foo.jpg/shahash
         // $params = ['w' => 100,   'h' => 200, etc]
+
+        // The name of the bin with default flags to make them run
         $jpegoptim = "jpegoptim -s ";
         $jpegtran = "jpegtran -optimize -progressive ";
         $optipng = "optipng ";
@@ -21,21 +23,36 @@ class CompressorListener extends Listener
         $gifsicle = "gifsicle -O5 ";
         $filetype = "";
 
-        if ( strpos( $path, ".jpg" ) || strpos( $path, ".JPG" ) || strpos( $path, ".jpeg" ) || strpos( $path, ".JPEG" ) !== false ) {
+        // Test the path for the file extensions and assign a filetype
+        if ( strpos( $path, ".jpg" ) ||
+             strpos( $path, ".JPG" ) ||
+             strpos( $path, ".jpeg" ) ||
+             strpos( $path, ".JPEG" ) !== false ) {
             $filetype = "jpg";
         }
-        elseif ( strpos( $path, ".png" ) || strpos( $path, ".PNG" ) !== false ) {
+        elseif ( strpos( $path, ".png" ) ||
+                 strpos( $path, ".PNG" ) !== false ) {
             $filetype = "png";
         }
-        elseif ( strpos( $path, ".gif" ) || strpos( $path, ".GIF" ) !== false ) {
+        elseif ( strpos( $path, ".gif" ) ||
+                 strpos( $path, ".GIF" ) !== false ) {
             $filetype = "gif";
         }
 
         if ( $filetype === "jpg" ) {
+            // Copy the existing glide-generated image to a tempory location and
+            // add a file extension. Some of the libraries have trouble if the
+            // file extension is not in place.
             $temp = $path . ".jpg";
             exec("cp " . $path . " " . $temp);
+
+            // Run jpegoptim and jpegtran
             exec($jpegoptim . "-t " . $temp);
             exec($jpegtran . $temp);
+
+            // Move the optimized file back to the original file location
+            // This gets rid of the extra file and should be a safe operation
+            // in case of the temp file never being generated.
             exec("mv " . $temp . " " . $path);
         }
         if ( $filetype === "png" ) {
